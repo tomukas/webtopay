f70f8e26d3219f7c2428da32e78d5d01b2e25ce3
Povilas Jurčys <povilas.jurcys@necolt.com>
2014-02-20 09:26:29 +0200

    Do not output html if we do not specify that with macro_form

--- lib/webtopay_helper.rb
+++ lib/webtopay_helper.rb
@@ -1,19 +1,24 @@
 module WebToPayHelper
   def macro_form(params, &block)
-    fields = {:orderid => "1", :lang => 'LIT', :amount => 0, :currency => 'LTL',
-              :accepturl => 'http://google.com', :cancelurl => 'http://yahoo.com', :callbackurl => 'http://yourdomain.com',
-              :projectid => WebToPay.config.project_id,
-              :sign_password => WebToPay.config.sign_password,
-              :test => 0}
+    fields = {
+        :orderid => "1", :lang => 'LIT', :amount => 0, :currency => 'LTL',
+        :projectid => WebToPay.config.project_id,
+        :sign_password => WebToPay.config.sign_password,
+        :test => 0
+    }
 
     fields.merge!(params)
 
     request = WebToPay::Api.build_request(fields)
 
-    concat "<form action=\"https://www.mokejimai.lt/pay/\" method=\"post\" style=\"padding:0px;margin:0px\">".html_safe
-    request.each_pair {|k,v| concat hidden_field_tag(k, v) if !v.nil? }
-    yield if block_given?
-    concat "</form>".html_safe
+    html = "<form action=\"https://www.mokejimai.lt/pay/\" method=\"post\" style=\"padding:0px;margin:0px\">"
+    request.each_pair do |field_name, field_value|
+      html << hidden_field_tag(field_name, field_value) unless field_value.nil?
+    end
+    html << capture(&block) if block_given?
+
+    html << "</form>"
+    html.html_safe
   end
 end
 
