0b3e5e4509ec44b995d26e819111e75a5fa3293d
Povilas Jurčys <povilas.jurcys@necolt.com>
2014-03-04 09:01:32 +0200

    Fix tests

--- spec/lib/webtopay_controller_spec.rb
+++ spec/lib/webtopay_controller_spec.rb
@@ -5,18 +5,12 @@ describe PaymentsController, type: :controller do
   describe '.webtopay' do
     it 'ignores methods that are not mentioned on webtopay actions list' do
       get :index
-
       # if webtopay is not ignored then then we should get message "'projectid' is required but missing." with 500 status code
       response.should be_success 
     end
 
     context "when webtopay params are missing" do
-      it 'should not be successfull' do
-        get :order
-        response.should_not be_success  
-      end
-
-      it 'displays error message' do
+      it 'raises error message' do
         expect { get :order }.to raise_error('"projectid" is invalid. Expected "12345", but was ""')
       end
     end
